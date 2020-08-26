042aa7b58fa352b3b002e4ebeec75655fc55ab01
Vinu <vinu.da2@iiitmk.ac.in>
2020-08-26 22:09:54 +0530

    Add code for PDS/week4 Ex_04_02

--- Python Data Structures/week4/Ex_04_02.py
+++ Python Data Structures/week4/Ex_04_02.py
@@ -23,4 +23,11 @@ if len(fname) < 1 : fname = "mbox-short.txt"
 fh = open(fname)
 count = 0
 
+for line in fh:
+    if line.startswith('From '):
+        from_line = line.split()
+        email = from_line[1]
+        print(email)
+        count = count + 1
+
 print("There were", count, "lines in the file with From as the first word")
