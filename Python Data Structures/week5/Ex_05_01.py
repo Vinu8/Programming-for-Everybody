9b73390c41aa02747e667ac189072125c7bcbd50
Vinu <vinu.da2@iiitmk.ac.in>
2020-08-28 18:16:38 +0530

    Add code for PDS/week5 Ex_05_01

--- Python Data Structures/week5/Ex_05_01.py
+++ Python Data Structures/week5/Ex_05_01.py
@@ -10,6 +10,55 @@ through the dictionary using a maximum loop to find the most
 prolific committer.
 
 '''
+'''
 name = input("Enter file:")
 if len(name) < 1 : name = "mbox-short.txt"
 handle = open(name)
+
+mail_list = []
+mail_dict = {}
+biggest = None
+
+for line in handle:
+    if line.startswith('From '):
+        from_line = line.split()
+        email = from_line[1]
+        mail_list.append(email)
+
+        for email in mail_list:
+            mail_dict[email] = mail_dict.get(email, 0) + 1
+        
+for key, value in mail_dict.items():
+    if biggest == None or value > biggest:
+        biggest = value
+        freq_mail = key
+
+print(freq_mail, biggest)
+'''
+
+# Refactored code
+
+name = input("Enter file:")
+if len(name) < 1 : name = "mbox-short.txt"
+handle = open(name)
+
+email_ids = []
+email_dict = dict()
+
+for line in handle:
+    if line.startswith('From '):
+        mail_list = line.split()
+        email_ids.append(mail_list[1])
+
+for i in email_ids:
+    email_dict[i] = email_dict.get(i, 0) + 1
+
+big_word = None
+big_count = None
+for key, value in email_dict.items():
+    if big_count is None or value > big_count:
+        big_count = value
+        big_word = key
+
+print(big_word, big_count)
+         
