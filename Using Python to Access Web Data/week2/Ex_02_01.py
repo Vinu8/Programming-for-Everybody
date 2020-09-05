ee9ba05aa45b19ec264406cfbde8e1e5e82a7fcc
Vinu <vinu.da2@iiitmk.ac.in>
2020-09-05 18:15:26 +0530

    Add code for UPAWD/week2 Ex_02_01

--- Using Python to Access Web Data/week2/Ex_02_01.py
+++ Using Python to Access Web Data/week2/Ex_02_01.py
@@ -3,4 +3,22 @@
 
 In this assignment you will read through and parse a file with text and numbers. You will extract all the numbers in the file and compute the sum of the numbers. 
 '''
+import re
 
+name = input('Enter file name : ')
+if len(name) < 1 : name = 'regex_sum_42.txt'
+handle = open(name)
+
+sum = 0
+num_list = []
+
+for line in handle:
+    line = line.rstrip()
+    num = re.findall('[0-9]+', line)
+    if len(num) < 1 : continue
+    for i in num:
+        num_list.append(int(i))
+
+for i in num_list:
+    sum = sum + i
+print(sum)
